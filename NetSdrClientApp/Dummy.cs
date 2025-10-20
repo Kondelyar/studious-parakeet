33216de422441c997ba3d3439dabd5f207938bed
Michael <Michael@DESKTOP-CQ31GTJ>
2025-10-20 05:20:21 +0300

    Додано пряму залежність між NetSdrClientApp і EchoTspServer. Створено клас Dummy у UI для демонстрації цієї залежності. Додано тестовий клас ArchitectureTests з правилом перевірки, щоб тест спеціально провалювався при наявності прямої залежності.

---
+++ NetSdrClientApp/Dummy.cs
@@ -0,0 +1,12 @@
+﻿using EchoTspServer;
+
+namespace NetSdrClientApp
+{
+    public class Dummy
+    {
+        public void CreateServer()
+        {
+            var server = new EchoServer(5000); // ось тут залежність на інфраструктуру
+        }
+    }
+}
